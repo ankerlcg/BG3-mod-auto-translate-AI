de89aa12dbce2e0a0155b654ccc5452ecca5e6d7
cklllll <76844519+ankerlcg@users.noreply.github.com>
2025-09-09 22:07:05 +0800

    调用LSLib.dll项目实现解包打包操作的转义DLL
    不知道为什么我直接Python调用LSLib.dll老是莫名其妙的错误,所以C#制作了个转义DLL方便打包
    
    LsLib项目链接:https://github.com/Norbyte/lslib

---
+++ unBG3Pack/unBG3Pack/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BG3Pack;
+
+
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BG3Pack.PackageManage.unPackageFile("C:\\Users\\ankerlcg\\PycharmProjects\\Bg3Mod\\test\\test.pak", "C:\\Users\\ankerlcg\\PycharmProjects\\Bg3Mod\\mod-unpak\\");
+            //BG3Pack.PackageManage.createPackage("C:\\Users\\ankerlcg\\PycharmProjects\\Bg3Mod\\mod-unpak\\act1_capes_139ee212-9e2c-78ef-7pgb", "C:\\Users\\ankerlcg\\PycharmProjects\\Bg3Mod\\mod-unpak\\act1_capes_139ee212-9e2c-78ef-7pgb\\1.pak");
+        }
+    }
+}
---
+++ unBG3Pack/unBG3Pack/PackageManage.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using LSLib.LS;
+using LSLib.LS.Enums;
+
+namespace BG3Pack
+{
+    public class PackageManage
+    {
+        public static void UnPackageFile(string packagePath,string unpackageDir)
+        {
+            LSLib.LS.Packager pkg = new LSLib.LS.Packager();
+            pkg.UncompressPackage(packagePath, unpackageDir);
+        }
+
+
+        public static void CreatePackage(string dataDir,string outPath)
+        {
+            var options = new PackageCreationOptions();
+            options.Version = PackageVersion.V18;
+            options.Compression = CompressionMethod.LZ4;
+            options.FastCompression = false;
+            options.Flags |= PackageFlags.Solid;
+            options.Flags |= PackageFlags.AllowMemoryMapping;
+            options.Flags |= PackageFlags.Preload;
+            decimal a = 0;
+            options.Priority = (byte)a;
+            var packager = new Packager();
+            packager.CreatePackage(outPath, dataDir, options);
+
+        }
+
+    }
+}
